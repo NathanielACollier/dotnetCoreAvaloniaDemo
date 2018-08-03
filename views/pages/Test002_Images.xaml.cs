75f5f6fd6dd2a68666d71f684c44cc62ac4b57c4
Nathaniel Collier <nathaniel.collier@aecc.com>
2018-08-03 13:30:57 -0500

    imageTesting:Display an image of the sun

---
+++ views/pages/Test002_Images.xaml.cs
@@ -0,0 +1,15 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace dotnetCoreAvaloniaDemo.views.pages{
+    public class Test002_Images: UserControl{
+
+        public Test002_Images(){
+            AvaloniaXamlLoader.Load(this);
+            
+        }
+
+    }
+}
