36d36604cc160d88d9885ce42ee32ffbc99b7a71
Nathaniel Collier <drxs3v3n@gmail.com>
2019-08-11 14:47:02 -0500

    Start example of building a window in code

---
+++ example2_windowFromCode/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Avalonia;
+
+namespace example2_windowFromCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new Application();
+            AppBuilder.Configure(app)
+                .UsePlatformDetect()
+                .SetupWithoutStarting();
+
+            var win = new Avalonia.Controls.Window();
+            var tb = new Avalonia.Controls.TextBlock
+            {
+                Text = "Hello World!"
+            };
+            win.Content = tb;
+            app.Run(win);
+        }
+    }
+}
